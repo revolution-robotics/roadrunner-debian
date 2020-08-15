f84f20c65671be7b0e9177042e13b35444c495ff
Andrew L. Moore <slewsys@gmail.com>
2020-08-15 05:45:14 +0000

    REVO i.MX7D: Restore python 2.7, needed by Debian.

--- revo/recoveryfs.sh
+++ revo/recoveryfs.sh
@@ -481,7 +481,7 @@ pip3 install pystemd
 pip3 install pytz
 
 # BEGIN -- REVO i.MX7D purge
-apt-get -y purge build-essential gcc-8 libpython2.7 libx11-6 manpages{,-dev}
+apt-get -y purge build-essential gcc-8 libx11-6 manpages{,-dev}
 apt-get --purge -y autoremove
 apt-get clean
 rm -r /var/lib/apt/lists/*
