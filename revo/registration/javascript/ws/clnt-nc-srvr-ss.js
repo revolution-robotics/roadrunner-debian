68e5e5c4d2f82cfcc336b5fdd591f4dd3c4e6f57
Andrew L. Moore <slewsys@gmail.com>
2020-10-02 04:50:53 +0000

    REVO i.MX7D: Update JavaScript with step CA certificates.

--- revo/registration/javascript/ws/clnt-nc-srvr-ss.js
+++ revo/registration/javascript/ws/clnt-nc-srvr-ss.js
@@ -8,9 +8,12 @@ const hostname = 'revotics.slewsys.org'
 const port = 30046;
 
 //
-// For server private certificates, disable certificate validation.
+// For some server self-signed certificates, disable certificate validation.
 //
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+// Add smallstep CA root certificate to certificate bundle.
+process.env.NODE_EXTRA_CA_CERTS = '/usr/share/certs/root_ca.crt';
 
 const ws = new WebSocket('wss://' + hostname + ':' + port);
 
