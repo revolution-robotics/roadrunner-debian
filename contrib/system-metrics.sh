182af11ba877cb3a12dc0491a759f2f2e46dc2af
Andrew L. Moore <slewsys@gmail.com>
2022-10-01 14:07:59 +0000

    REVO i.MX7D: Add script `flash-diskimage`.
    Clean up script `system-metrics.sh`.
    Append _CMD to environment path variables.

--- contrib/system-metrics.sh
+++ contrib/system-metrics.sh
@@ -2,104 +2,123 @@
 #
 # @(#) system-metrics.sh
 #
+# Copyright © 2022 Revolution Robotics, Inc.
+#
 # This script displays a summary of system metrics.
 #
-declare script_name=${0##*/}
-
 progress_bar ()
 {
-    declare -i runtime=${1:-60}
+    local -i runtime=${1:-60}
+    local -i update_interval=${2:-5}
 
-    declare dots=''
-    declare -i dots_max=40
-    declare -i interval=5
-    declare -i i=0
-    declare -i i_max=$(( (interval * 100 / runtime ) * (100 / (interval * 100 / runtime)) ))
-    declare -i columns=$(
+    local dots=''
+    local -i dots_max=40
+    local -i i=0
+
+    for (( i = 0; i < 100; i += update_interval * 100 / runtime )); do
+        dots=$(eval printf ".%.0s" {0..$(( dots_max * i / 100 ))})
+        printf "\r%-${dots_max}s(%2d%%)" "${dots%.}" "$i"
+        sleep "$update_interval"
+    done
+
+    local -i columns=0
+
+    columns=$(
         stty -a </dev/tty |
             sed -n -e '/columns/s/.*columns \([^ ]*\);.*/\1/p'
-            )
+            ) || return $?
 
-    while (( i < i_max )); do
-        sleep $interval
-        (( i += interval * 100 / runtime ))
-        dots=$(eval printf ".%.0s" {1..$(( dots_max * i / 100 ))})
-        printf "\r%-${dots_max}s(%2d%%)" "$dots" "$i"
-    done
     printf '\r'
     eval printf '=%.0s' {1..$columns}
 }
 
 collect_metrics ()
 {
-    echo >>"$tmpfile"
-    echo "**** BEGIN OUTPUT OF: uptime ****" >>"$tmpfile"
-    uptime >>"$tmpfile"
-    echo "**** END OUTPUT OF: uptime ****" >>"$tmpfile"
-    echo >>"$tmpfile"
+    local output_file=$1
 
-    echo "**** BEGIN OUTPUT OF: dmesg -T ****" >>"$tmpfile"
-    script -qc "dmesg -T" >> "$tmpfile"
-    echo "**** END OUTPUT OF: dmesg -T ****" >>"$tmpfile"
-    echo >>"$tmpfile"
 
-    echo "**** BEGIN OUTPUT OF: vmstat -a -SM 1 ****" >>"$tmpfile"
-    vmstat -a -SM 1 >>"$tmpfile" &
-    sleep 10
-    kill $!
-    echo "**** END OUTPUT OF: vmstat -a -SM 1 ****" >>"$tmpfile"
-    echo >>"$tmpfile"
+    echo >>"$output_file"
+    echo "**** BEGIN OUTPUT OF: uptime ****" >>"$output_file"
+    uptime >>"$output_file"  || return $?
+    echo "**** END OUTPUT OF: uptime ****" >>"$output_file"
+    echo >>"$output_file"
 
-    echo "**** BEGIN OUTPUT OF: mpstat -P ALL 2 5 ****" >>"$tmpfile"
-    script -qc "mpstat -P ALL 2 5" >>"$tmpfile"
-    echo "**** END OUTPUT OF: mpstat -P ALL 2 5 ****" >>"$tmpfile"
-    echo >>"$tmpfile"
+    echo "**** BEGIN OUTPUT OF: dmesg -T ****" >>"$output_file"
+    script -qc "dmesg -T" >> "$output_file"  || return $?
+    echo "**** END OUTPUT OF: dmesg -T ****" >>"$output_file"
+    echo >>"$output_file"
 
-    echo "**** BEGIN OUTPUT OF: mpstat -P ALL -I SCPU,SUM 2 5 ****" >>"$tmpfile"
-    script -qc "mpstat -P ALL -I SCPU,SUM 2 5" >>"$tmpfile"
-    echo "**** END OUTPUT OF: mpstat -P ALL -I SCPU,SUM 2 5 ****" >>"$tmpfile"
-    echo >>"$tmpfile"
+    echo "**** BEGIN OUTPUT OF: mpstat -P ALL 2 5 ****" >>"$output_file"
+    script -qc "mpstat -P ALL 2 5" >>"$output_file" || return $?
+    echo "**** END OUTPUT OF: mpstat -P ALL 2 5 ****" >>"$output_file"
+    echo >>"$output_file"
 
-    echo "**** BEGIN OUTPUT OF: pidstat 2 5 ****" >>"$tmpfile"
-    script -qc "pidstat 2 5" >>"$tmpfile"
-    echo "**** END OUTPUT OF: pidstat 2 5 ****" >>"$tmpfile"
-    echo >>"$tmpfile"
+    echo "**** BEGIN OUTPUT OF: mpstat -P ALL -I SCPU,SUM 2 5 ****" >>"$output_file"
+    script -qc "mpstat -P ALL -I SCPU,SUM 2 5" >>"$output_file" || return $?
+    echo "**** END OUTPUT OF: mpstat -P ALL -I SCPU,SUM 2 5 ****" >>"$output_file"
+    echo >>"$output_file"
 
-    echo "**** BEGIN OUTPUT OF: iostat -sxzh 2 5 ****" >>"$tmpfile"
-    script -qc "iostat -sxzh 2 5" >>"$tmpfile"
-    echo "**** END OUTPUT OF: iostat -sxzh 2 5 ****" >>"$tmpfile"
-    echo >>"$tmpfile"
+    echo "**** BEGIN OUTPUT OF: pidstat 2 5 ****" >>"$output_file"
+    script -qc "pidstat 2 5" >>"$output_file" || return $?
+    echo "**** END OUTPUT OF: pidstat 2 5 ****" >>"$output_file"
+    echo >>"$output_file"
 
-    echo "**** BEGIN OUTPUT OF: free -m ****" >>"$tmpfile"
-    free -m >>"$tmpfile"
-    echo "**** END OUTPUT OF: free -m ****" >>"$tmpfile"
-    echo >>"$tmpfile"
+    echo "**** BEGIN OUTPUT OF: iostat -sxzh 2 5 ****" >>"$output_file"
+    script -qc "iostat -sxzh 2 5" >>"$output_file" || return $?
+    echo "**** END OUTPUT OF: iostat -sxzh 2 5 ****" >>"$output_file"
+    echo >>"$output_file"
 
-    echo "**** BEGIN OUTPUT OF: sar -n DEV 2 5 ****" >>"$tmpfile"
-    script -qc "sar -n DEV 2 5" >>"$tmpfile"
-    echo "**** END OUTPUT OF: sar -n DEV 2 5 ****" >>"$tmpfile"
-    echo >>"$tmpfile"
+    echo "**** BEGIN OUTPUT OF: free -m ****" >>"$output_file"
+    free -m >>"$output_file" || return $?
+    echo "**** END OUTPUT OF: free -m ****" >>"$output_file"
+    echo >>"$output_file"
 
-    echo "**** BEGIN OUTPUT OF: sar -n TCP,ETCP 2 5 ****" >>"$tmpfile"
-    script -qc "sar -n TCP,ETCP 2 5" >>"$tmpfile"
-    echo "**** BEGIN OUTPUT OF: sar -n TCP,ETCP 2 5 ****" >>"$tmpfile"
-    echo >>"$tmpfile"
+    echo "**** BEGIN OUTPUT OF: sar -n DEV 2 5 ****" >>"$output_file"
+    script -qc "sar -n DEV 2 5" >>"$output_file" || return $?
+    echo "**** END OUTPUT OF: sar -n DEV 2 5 ****" >>"$output_file"
+    echo >>"$output_file"
 
-    echo "$tmpfile"
+    echo "**** BEGIN OUTPUT OF: sar -n TCP,ETCP 2 5 ****" >>"$output_file"
+    script -qc "sar -n TCP,ETCP 2 5" >>"$output_file" || return $?
+    echo "**** END OUTPUT OF: sar -n TCP,ETCP 2 5 ****" >>"$output_file"
+    echo >>"$output_file"
+
+    echo "**** BEGIN OUTPUT OF: vmstat -a -SM 1 10 ****" >>"$output_file"
+    vmstat -a -SM 1 10 >>"$output_file"  || return $?
+    echo "**** END OUTPUT OF: vmstat -a -SM 1 10 ****" >>"$output_file"
+    echo >>"$output_file"
 }
 
 if test ."$0" = ."${BASH_SOURCE[0]}"; then
-    declare -i runtime=68
-    declare tmpfile=$(mktemp -p /tmp "${script_name}.XXXXX")
+    declare script_name=${0##*/}
+    declare -i runtime=60
+    declare -i update_interval=5
+
+    declare tmpfile=''
+
+    tmpfile=$(mktemp -p /tmp "${script_name}.XXXXXXX") || exit $?
 
     trap 'rm -f "$tmpfile"; exit' 0 1 2 15
 
-    echo "Collectings metrics, please wait..."
-    progress_bar $runtime &
-    declare -i begin=$(date +%s)
-    declare metrics=$(collect_metrics)
-    declare -i end=$(date +%s)
-    echo "Runtime: $(( end - begin ))s" >>"$metrics"
-    wait
-    less -R <"$metrics"
+    echo "Collecting metrics, please wait..."
+
+    declare -i start_time=0
+    declare -i progress_bar_pid=0
+
+    start_time=$(date +%s) || exit 1
+    progress_bar "$runtime" "$update_interval" &
+    progress_bar_pid=$!
+    collect_metrics "$tmpfile" || { kill "$progress_bar_pid"; exit 1; }
+
+    declare -i stop_time=0
+
+    stop_time=$(date +%s) || exit 1
+    actual_runtime=$(( stop_time - start_time ))
+    printf "${script_name} runtime: ${actual_runtime}\n" >>"$tmpfile"
+
+    if (( runtime > actual_runtime )); then
+        sleep $(( runtime - actual_runtime ))
+    fi
+
+    less -R <"$tmpfile"
 fi
