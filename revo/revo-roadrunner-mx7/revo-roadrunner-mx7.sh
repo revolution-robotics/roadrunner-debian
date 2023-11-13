25b9e18c84af7440fafa309047257b2b6c423c64
Andrew L. Moore <slewsys@gmail.com>
2023-11-12 21:40:41 -0500

    REVO i.MX7D: Sync with Wireguard kernel update.

--- revo/revo-roadrunner-mx7/revo-roadrunner-mx7.sh
+++ revo/revo-roadrunner-mx7/revo-roadrunner-mx7.sh
@@ -26,7 +26,7 @@ declare -r G_LINUX_KERNEL_GIT=https://github.com/revolution-robotics/roadrunner-
 # declare -r G_LINUX_KERNEL_BRANCH=imx_5.4.85_1.0.0_revo
 # declare -r G_LINUX_KERNEL_REV=9292229fa9587f0587541bc2962685a442cbd6db
 declare -r G_LINUX_KERNEL_BRANCH=imx_5.4.142_1.0.0_revo_us
-declare -r G_LINUX_KERNEL_REV=27a4bc97d510e392a1582556fcfcdcba12654097
+declare -r G_LINUX_KERNEL_REV=65e6a9595dc635d5b0aeb084ef33dad5f4982367
 declare -r G_LINUX_KERNEL_DEF_CONFIG=imx_v7_roadrunner_defconfig
 declare -r G_LINUX_DTB='imx7d-roadrunner-gpio16.dtb imx7d-roadrunner-iomix.dtb imx7d-roadrunner-blade.dtb'
 
